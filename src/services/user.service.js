bd48099f11951f7fb315049e88cc8b6fb7718a1f
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-10-04 14:29:30 -0300

    DEL create user services

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,24 +3,6 @@ import User from '../entity/user.entity.js';
 import { AppDataSource } from '../config/configDb.js';
 import { formatToLocalTime } from '../utils/formatDate.js'
 
-export async function createUserService(dataUser) {
-    try {
-        const userRepository = AppDataSource.getRepository(User);
-
-        const newUser = userRepository.create({
-            nombreCompleto: dataUser.nombreCompleto,
-            rut: dataUser.rut,
-            email: dataUser.email
-        });
-
-        const userSaved = await userRepository.save(newUser);
-
-        return userSaved;
-    } catch (error) {
-        console.error('Error al crear un usuario: ', error);
-    }
-}
-
 export async function getUserService(id) {
     try {
         const userRepository = AppDataSource.getRepository(User);
